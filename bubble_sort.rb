9b3e639d626dcc8f98e9d38618a75bcb8a58158b
serdg0 <sergio.diazg92@gmail.com>
2019-07-18 14:57:11 -0500

    protocol

---
+++ bubble_sort.rb
@@ -0,0 +1,44 @@
+arr = [5,6,4,1,3,2,7,9,8]
+strings = ["hi", "hello", "hey"]
+
+module Enumerable
+
+    def bubble_sort
+        i=self.size
+        while i >= 0
+            j = 0
+            while j <= i
+                if self[j+1] != nil && self[j+1] < self[j]
+                    swapper = self[j]
+                    self[j] = self[j+1]
+                    self[j+1] = swapper
+                end
+                j+=1
+            end
+            i-=1
+        end
+        self
+    end
+
+    def bubble_sort_by
+        i=self.size
+        while i >= 0
+            j = 0
+            while j <= i
+                if self[j+1] != nil && yield(self[j],self[j+1]) > 0
+                    swapper = self[j]
+                    self[j] = self[j+1]
+                    self[j+1] = swapper
+                end
+                j+=1
+            end
+            i-=1
+        end
+        self
+    end 
+
+end
+
+puts strings.bubble_sort_by{|x,y| x.length - y.length}
+print arr.bubble_sort
+
